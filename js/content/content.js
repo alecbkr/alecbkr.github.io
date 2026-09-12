3d0cc8f7c516f198122cec57c1b2c85985253e3b
Alec Baker <alecbakr@gmail.com>
2026-09-12 01:48:27 -0400

    refinements

--- js/content/content.js
+++ js/content/content.js
@@ -19,7 +19,7 @@ export function openContent(contentId) {
 
         contentContainer.style.width = selectedContent.dataset.width;
         contentTitle.textContent = selectedContent.dataset.title;
-        window.location.hash = contentId;
+        history.pushState(null, "", `${contentId}`);
     }
 }
 
